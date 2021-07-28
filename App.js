a1b1ee54a608bee56c753ef6f5f297b2cb9406f8
Villee00 <ville.2000.vhk@gmail.com>
2021-07-28 22:10:20 +0300

    10.11

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
 
 import Main from './Main';
+import createApolloClient from './src/utils/apolloClient';
 
+const apolloClient = createApolloClient();
 export default function App() {
   return (
   <NativeRouter>
-    <Main/>
+    <ApolloProvider client={apolloClient}>
+      <Main/>
+    </ApolloProvider>
   </NativeRouter>
   );
 }
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,20 @@
+import { useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const { data, refetch, loading} = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  useEffect(() => {
+    if(!loading){
+      setRepositories(data.repositories);
+    }
+  }, [data]);
+
+  return { repositories, loading, refetch };
+};
+
+export default useRepositories;
